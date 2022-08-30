9205840cd05dafc400dc58fb3eeffc9c77fb5646
duhui1004@naver.com <duhui1004@naver.com>
2022-08-30 10:44:18 +0900

    #1 - data rest 설정

--- src/main/java/com/project/board/repository/ArticleCommentRepository.java
+++ src/main/java/com/project/board/repository/ArticleCommentRepository.java
@@ -1,9 +1,11 @@
 package com.project.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.project.board.domain.ArticleComment;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long>{
     
 }
--- src/main/java/com/project/board/repository/ArticleRepository.java
+++ src/main/java/com/project/board/repository/ArticleRepository.java
@@ -1,9 +1,11 @@
 package com.project.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.project.board.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long>{
     
 }
